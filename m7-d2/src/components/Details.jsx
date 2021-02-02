0f5ac941d662f830f3cb31cf30892728026d1a11
elinoza <hillcakmak@gmail.com>
2021-02-02 19:08:36 +0300

    hh

---
+++ m7-d2/src/components/Details.jsx
@@ -0,0 +1,13 @@
+
+import React, { Component } from "react";
+
+class Details extends Component {
+    render(){
+        console.log( this.props.position)
+        return(
+
+            
+        )
+    }
+};
+export default Details
